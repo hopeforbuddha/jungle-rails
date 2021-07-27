30a69333bd508df2ffe300cae2970521cda45444
Jacob <jacob.fitzgerald@outlook.com>
2021-07-27 17:35:37 +0000

    add user auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get'/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get "about" => "about#show"
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
